d09761f2fc84063d4694a682f534f07bce2ca1ee
Malte Hübner <maltehuebner@gmx.org>
2016-12-13 23:15:40 +0100

    Added return type.

--- src/Caldera/Bundle/CyclewaysBundle/Controller/FrontpageController.php
+++ src/Caldera/Bundle/CyclewaysBundle/Controller/FrontpageController.php
@@ -2,11 +2,12 @@
 
 namespace Caldera\Bundle\CyclewaysBundle\Controller;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class FrontpageController extends AbstractController
 {
-    public function indexAction(Request $request)
+    public function indexAction(Request $request): RedirectResponse
     {
         return $this->redirectToRoute(
             'caldera_cycleways_incident_map_city',
