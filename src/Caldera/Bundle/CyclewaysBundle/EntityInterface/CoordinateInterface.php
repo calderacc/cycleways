f086ed71eb391146add63249c48a99dacbb66eec
Malte Hübner <maltehuebner@gmx.org>
2016-12-14 00:15:53 +0100

    Copied some interfaces.

---
+++ src/Caldera/Bundle/CyclewaysBundle/EntityInterface/CoordinateInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Caldera\Bundle\CalderaBundle\EntityInterface;
+
+interface CoordinateInterface
+{
+    public function setLatitude($latitude);
+
+    public function getLatitude();
+
+    public function setLongitude($longitude);
+
+    public function getLongitude();
+}
---
+++ src/Caldera/Bundle/CyclewaysBundle/EntityInterface/ViewableInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Caldera\Bundle\CalderaBundle\EntityInterface;
+
+interface ViewableInterface
+{
+    public function getId();
+
+    public function getViews();
+
+    public function setViews($views);
+
+    public function incViews();
+}
