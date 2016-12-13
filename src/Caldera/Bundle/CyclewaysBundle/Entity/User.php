c4e82445cc435fa488eed01d0e9237a928b74ca5
Malte Hübner <maltehuebner@gmx.org>
2016-12-14 00:19:19 +0100

    Change user table name.

--- src/Caldera/Bundle/CyclewaysBundle/Entity/User.php
+++ src/Caldera/Bundle/CyclewaysBundle/Entity/User.php
@@ -9,7 +9,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="fos_user_user")
+ * @ORM\Table(name="user")
  * @ORM\HasLifecycleCallbacks
  * @JMS\ExclusionPolicy("all")
  */
