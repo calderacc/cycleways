f0ac9d51c94555a8d085830cd04b55b82aac2fc8
Malte Hübner <maltehuebner@gmx.org>
2016-12-19 02:45:46 +0100

    Fix return types.

--- src/Caldera/Bundle/CyclewaysBundle/Repository/PhotoRepository.php
+++ src/Caldera/Bundle/CyclewaysBundle/Repository/PhotoRepository.php
@@ -7,19 +7,12 @@ use Doctrine\ORM\EntityRepository;
 
 class PhotoRepository extends EntityRepository
 {
-    public function getPreviousPhoto(Photo $photo)
+    public function getPreviousPhoto(Photo $photo): ?Photo
     {
         $builder = $this->createQueryBuilder('photo');
 
         $builder->select('photo');
-
-        if ($photo->getRide()) {
-            $builder->where($builder->expr()->eq('photo.ride', $photo->getRide()->getId()));
-        } elseif ($photo->getEvent()) {
-            $builder->where($builder->expr()->eq('photo.event', $photo->getEvent()->getId()));
-        } elseif ($photo->getIncident()) {
-            $builder->where($builder->expr()->eq('photo.incident', $photo->getIncident()->getId()));
-        }
+        $builder->where($builder->expr()->eq('photo.incident', $photo->getIncident()->getId()));
 
         $builder->andWhere($builder->expr()->lt('photo.dateTime', '\'' . $photo->getDateTime()->format('Y-m-d H:i:s') . '\''));
         $builder->andWhere($builder->expr()->eq('photo.enabled', 1));
@@ -35,26 +28,13 @@ class PhotoRepository extends EntityRepository
         return $result;
     }
 
-    /**
-     * @param Photo $photo
-     * @return Photo
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     * @author maltehuebner
-     * @since 2015-12-06
-     */
-    public function getNextPhoto(Photo $photo)
+    public function getNextPhoto(Photo $photo): ?Photo
     {
         $builder = $this->createQueryBuilder('photo');
 
         $builder->select('photo');
 
-        if ($photo->getRide()) {
-            $builder->where($builder->expr()->eq('photo.ride', $photo->getRide()->getId()));
-        } elseif ($photo->getEvent()) {
-            $builder->where($builder->expr()->eq('photo.event', $photo->getEvent()->getId()));
-        } elseif ($photo->getIncident()) {
-            $builder->where($builder->expr()->eq('photo.incident', $photo->getIncident()->getId()));
-        }
+        $builder->where($builder->expr()->eq('photo.incident', $photo->getIncident()->getId()));
 
         $builder->andWhere($builder->expr()->gt('photo.dateTime', '\'' . $photo->getDateTime()->format('Y-m-d H:i:s') . '\''));
         $builder->andWhere($builder->expr()->eq('photo.enabled', 1));
--- src/Caldera/Bundle/CyclewaysBundle/Repository/PostRepository.php
+++ src/Caldera/Bundle/CyclewaysBundle/Repository/PostRepository.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\EntityRepository;
 
 class PostRepository extends EntityRepository
 {
-    public function getPostsForIncident(Incident $incident)
+    public function getPostsForIncident(Incident $incident): array
     {
         $builder = $this->createQueryBuilder('post');
 
