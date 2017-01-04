b3586b1c57994a66bca2f1a963c4369d5d6c12b6
Malte Hübner <maltehuebner@gmx.org>
2017-01-05 00:57:46 +0100

    Fix slug generation.

--- src/Caldera/Bundle/CyclewaysBundle/SlugGenerator/SlugGenerator.php
+++ src/Caldera/Bundle/CyclewaysBundle/SlugGenerator/SlugGenerator.php
@@ -13,7 +13,19 @@ class SlugGenerator
 
     public function generateSlug(Incident $incident): string
     {
-        $slug = new Slug($incident->getTitle().' '.$incident->getStreet().' '.$incident->getDistrict().' '.$incident->getTown().' '.$incident->getId());
+        $slugArray = [];
+        $slugArray[] = $incident->getTitle();
+        $slugArray[] = $incident->getStreet() ?? null;
+        $slugArray[] = $incident->getDistrict() ?? null;
+        $slugArray[] = $incident->getSuburb() ?? null;
+        $slugArray[] = $incident->getTown() ?? null;
+        $slugArray[] = $incident->getVillage() ?? null;
+        $slugArray[] = $incident->getCity() ?? null;
+        $slugArray[] = $incident->getId();
+
+        $slugString = implode('-', array_filter($slugArray));
+
+        $slug = new Slug($slugString);
         $incident->setSlug($slug);
 
         return $slug;
