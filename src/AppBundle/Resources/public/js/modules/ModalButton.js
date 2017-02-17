0760807db71cf7a51379c3bcf477d0c54610e78c
Malte Hübner <maltehuebner@gmx.org>
2017-02-17 20:53:29 +0100

    WIP.

--- src/AppBundle/Resources/public/js/modules/ModalButton.js
+++ src/AppBundle/Resources/public/js/modules/ModalButton.js
@@ -2,49 +2,54 @@ define([], function () {
     ModalButton = function () {
     };
 
-    ModalButton.prototype._caption = '';
-    ModalButton.prototype._icon = '';
-    ModalButton.prototype._link = '';
-    ModalButton.prototype._type = '';
     ModalButton.prototype._$button = null;
+    ModalButton.prototype._html = null;
+
+    ModalButton.prototype._icon = null;
+    ModalButton.prototype._caption = null;
+    ModalButton.prototype._class = null;
+    ModalButton.prototype._href = null;
+
+    ModalButton.prototype._onClickEventCallback = null;
 
     ModalButton.prototype.setCaption = function (caption) {
         this._caption = caption;
+    };
+
+    ModalButton.prototype.setHref = function (href) {
+        this._href = href;
+    };
+
+    ModalButton.prototype.setClass = function (btnClass) {
+        this._class = btnClass;
     };
 
     ModalButton.prototype.setIcon = function (icon) {
         this._icon = icon;
     };
 
-    ModalButton.prototype.setLink = function (link) {
-        this._link = link;
+    ModalButton.prototype.setOnClickEvent = function (callback) {
+        this._onClickEventCallback = callback;
     };
 
-    ModalButton.prototype.setType = function (type) {
-        this._type = type;
-    };
+    ModalButton.prototype.render = function () {
+        var icon = '';
 
-    ModalButton.prototype._build = function () {
-        this._$button = $([
-            '<div class="modal fade" tabindex="-1" role="dialog">',
-            '  <div class="modal-dialog modal-' + this._size + '">',
-            '    <div class="modal-content">',
-            '      <div class="modal-header">',
-            '        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>',
-            '        <h4 class="modal-title">',
-            '          ' + this._modalTitle,
-            '        </h4>',
-            '      </div>',
-            '      <div class="modal-body">',
-            '        ' + this._modalBody,
-            '      </div>',
-            '      <div class="modal-footer">',
-            '        ' + this._modalFooter,
-            '      </div>',
-            '    </div>',
-            '  </div>',
-            '</div>'
-        ].join("\n"));
+        if (this._icon) {
+            icon = '<i class="fa fa-' + this._icon + '" aria-hidden="true"></i> ';
+        }
+
+        if (this._href) {
+            this._html = '<a class="btn ' + this._class + '" href="' + this._href + '">' + icon + this._caption + '</a>';
+        } else {
+            this._html = '<button class="btn ' + this._class + '">' + icon + this._caption + '</button>';
+        }
+
+        this._$button = $(this._html);
+
+        this._$button.on('click', this._onClickEventCallback);
+
+        return this._$button;
     };
 
     return ModalButton;
