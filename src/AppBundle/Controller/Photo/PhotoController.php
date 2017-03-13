cbb1796cfc54b4e0854a083fb78a1b062552a8d6
Malte Hübner <maltehuebner@gmx.org>
2017-03-13 20:11:34 +0100

    Split PhotoController in two new controller.

--- src/AppBundle/Controller/Photo/PhotoController.php
+++ src/AppBundle/Controller/Photo/PhotoController.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Controller\Photo;
 
 use AppBundle\Controller\AbstractController;
-use AppBundle\Entity\Incident;
 use AppBundle\Entity\Photo;
 use AppBundle\Traits\ViewStorageTrait;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,49 +11,6 @@ use Symfony\Component\HttpFoundation\Response;
 class PhotoController extends AbstractController
 {
     use ViewStorageTrait;
-
-    public function uploadAction(Request $request, string $slug): Response
-    {
-        $incident = $this->getIncidentRepository()->findOneBySlug($slug);
-
-        if (!$incident) {
-            throw $this->createNotFoundException();
-        }
-
-        if ($request->getMethod() == 'POST') {
-            return $this->uploadPostAction($request, $incident);
-        } else {
-            return $this->uploadGetAction($request, $incident);
-        }
-    }
-
-    protected function uploadGetAction(Request $request, Incident $incident): Response
-    {
-        return $this->render(
-            'AppBundle:Photo:upload.html.twig',
-            [
-                'incident' => $incident
-            ]
-        );
-    }
-
-    protected function uploadPostAction(Request $request, Incident $incident): Response
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $photo = new Photo();
-
-        $photo->setImageFile($request->files->get('file'));
-        $photo->setUser($this->getUser());
-
-        $photo->setIncident($incident);
-        //$photo->setCity($incident->getCity());
-
-        $em->persist($photo);
-        $em->flush();
-
-        return new Response('foo');
-    }
 
     public function showAction(Request $request, string $slug, int $photoId): Response
     {
