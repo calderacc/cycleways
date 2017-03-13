fe7b9572c701da1d4c203be2c0016d29a2a863d3
Malte Hübner <maltehuebner@gmx.org>
2017-03-13 20:38:00 +0100

    Allow to set a featured photo for incidents.

---
+++ src/AppBundle/Controller/Photo/ManagementController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Controller\Photo;
+
+use AppBundle\Controller\AbstractController;
+use AppBundle\Entity\Incident;
+use AppBundle\Entity\Photo;
+use AppBundle\Traits\ViewStorageTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ManagementController extends AbstractController
+{
+    public function featuredPhotoAction(Request $request, UserInterface $user, int $photoId): RedirectResponse
+    {
+        $photo = $this->getPhotoRepository()->find($photoId);
+
+        if (!$photo) {
+            throw $this->createNotFoundException();
+        }
+
+        $incident = $photo->getIncident();
+        $incident->setFeaturedPhoto($photo);
+
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->redirectToRoute(
+            'caldera_cycleways_incident_show',
+            [
+              'slug' => $incident->getSlug()
+            ]
+        );
+    }
+}
