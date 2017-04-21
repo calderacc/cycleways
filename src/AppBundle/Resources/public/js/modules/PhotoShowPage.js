39312976a5f396e4d335933126f2775e75c3efe2
Malte Hübner <maltehuebner@gmx.org>
2017-04-21 09:28:57 +0200

    Toggle image and detail display.

--- src/AppBundle/Resources/public/js/modules/PhotoShowPage.js
+++ src/AppBundle/Resources/public/js/modules/PhotoShowPage.js
@@ -1,9 +1,29 @@
 define(['CriticalService'], function (CriticalService) {
     PhotoShowPage = function (context, options) {
-        alert('wfwefew');
+        this._initEventListeners();
     };
 
     PhotoShowPage.prototype._CriticalService = null;
+
+    PhotoShowPage.prototype._initEventListeners = function() {
+        $('#photo').click(this.toggleDisplay.bind(this));
+    };
+
+    PhotoShowPage.prototype.toggleDisplay = function() {
+        var $photoCol = $('#photo-col');
+        var $detailCol = $('#detail-col');
+
+        if ($detailCol.is(":visible")) {
+            $photoCol.removeClass('col-md-9').addClass('col-md-12');
+
+            $detailCol.hide();
+        } else {
+            $photoCol.removeClass('col-md-12').addClass('col-md-9');
+
+            $detailCol.show();
+        }
+
+    };
 
     return PhotoShowPage;
 });
