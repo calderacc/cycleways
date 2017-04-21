30f12a9a1b598ee70503cf491a037da10b200456
Malte Hübner <maltehuebner@gmx.org>
2017-04-21 09:24:12 +0200

    Getting started with PhotoShowPage.

---
+++ src/AppBundle/Resources/public/js/modules/PhotoShowPage.js
@@ -0,0 +1,9 @@
+define(['CriticalService'], function (CriticalService) {
+    PhotoShowPage = function (context, options) {
+        alert('wfwefew');
+    };
+
+    PhotoShowPage.prototype._CriticalService = null;
+
+    return PhotoShowPage;
+});
