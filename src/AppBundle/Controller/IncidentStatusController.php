5c4c1c713728f377044ddeba678fd8c7fb42937b
Malte Hübner <maltehuebner@gmx.org>
2017-05-15 15:23:14 +0200

    Show incident stauts list in frontend.

--- src/AppBundle/Controller/IncidentStatusController.php
+++ src/AppBundle/Controller/IncidentStatusController.php
@@ -10,6 +10,11 @@ class IncidentStatusController extends AbstractController
 {
     public function renderAction(Request $request, Incident $incident): Response
     {
-        return $this->render('AppBundle:IncidentStatus:list.html.twig');
+        $statusList = $this->getDoctrine()->getRepository('AppBundle:Status')->findAll();
+
+        return $this->render('AppBundle:IncidentStatus:list.html.twig', [
+            'statusList' => $statusList,
+            'incident' => $incident
+        ]);
     }
 }
