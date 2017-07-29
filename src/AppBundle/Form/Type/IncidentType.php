4f3bace9f759127213e2dfb3f7c805bd8a411d50
Malte Hübner <maltehuebner@gmx.org>
2017-07-29 21:36:30 +0200

    Do not require anything.

--- src/AppBundle/Form/Type/IncidentType.php
+++ src/AppBundle/Form/Type/IncidentType.php
@@ -21,8 +21,8 @@ class IncidentType extends AbstractType
         $builder
             ->add('title', TextType::class, ['required' => false])
             ->add('description', TextareaType::class, ['required' => false])
-            ->add('polyline', HiddenType::class)
-            ->add('geometryType', HiddenType::class)
+            ->add('polyline', HiddenType::class, ['required' => false])
+            ->add('geometryType', HiddenType::class, ['required' => false])
             ->add('incidentType', ChoiceType::class,
                 [
                     'choices' => [
@@ -38,6 +38,7 @@ class IncidentType extends AbstractType
             )
             ->add('dangerLevel', ChoiceType::class,
                 [
+                    'required' => false,
                     'choices' => [
                         'ungefährlich' => Incident::DANGER_LEVEL_NONE,
                         'niedrig' => Incident::DANGER_LEVEL_LOW,
@@ -51,33 +52,37 @@ class IncidentType extends AbstractType
                     'date_widget' => 'single_text',
                     'model_timezone' => 'UTC',
                     'view_timezone' => 'Europe/Berlin',
+                    'required' => false,
                 ])
             ->add('visibleFrom', DateType::class,
                 [
                     'widget' => 'single_text',
                     'model_timezone' => 'UTC',
                     'view_timezone' => 'Europe/Berlin',
+                    'required' => false,
                 ])
             ->add('visibleTo',  DateType::class,
                 [
                     'widget' => 'single_text',
                     'model_timezone' => 'UTC',
                     'view_timezone' => 'Europe/Berlin',
+                    'required' => false,
                 ])
-            ->add('expires', CheckboxType::class)
-            ->add('street', TextType::class)
-            ->add('houseNumber', TextType::class)
-            ->add('suburb', HiddenType::class)
-            ->add('district', HiddenType::class)
-            ->add('town', HiddenType::class)
-            ->add('village', HiddenType::class)
-            ->add('city', HiddenType::class)
-            ->add('zipCode', HiddenType::class)
-            ->add('streetviewLink', TextType::class)
-            ->add('latitude', HiddenType::class)
-            ->add('longitude', HiddenType::class)
+            ->add('expires', CheckboxType::class, ['required' => false])
+            ->add('street', TextType::class, ['required' => false])
+            ->add('houseNumber', TextType::class, ['required' => false])
+            ->add('suburb', HiddenType::class, ['required' => false])
+            ->add('district', HiddenType::class, ['required' => false])
+            ->add('town', HiddenType::class, ['required' => false])
+            ->add('village', HiddenType::class, ['required' => false])
+            ->add('city', HiddenType::class, ['required' => false])
+            ->add('zipCode', HiddenType::class, ['required' => false])
+            ->add('streetviewLink', TextType::class, ['required' => false])
+            ->add('latitude', HiddenType::class, ['required' => false])
+            ->add('longitude', HiddenType::class, ['required' => false])
             ->add('accidentType', ChoiceType::class,
                 [
+                    'required' => false,
                     'choices' => [
                         '' => null,
                         'unbekannt' => Incident::ACCIDENT_TYPE_UNKNOWN,
@@ -97,6 +102,7 @@ class IncidentType extends AbstractType
             )
             ->add('accidentLocation', ChoiceType::class,
                 [
+                    'required' => false,
                     'choices' => [
                         '' => null,
                         'innerorts' => Incident::ACCIDENT_LOCATION_CITY,
@@ -106,6 +112,7 @@ class IncidentType extends AbstractType
             )
             ->add('accidentInfrastructure', ChoiceType::class,
                 [
+                    'required' => false,
                     'choices' => [
                         '' => null,
                         'Fahrbahn' => Incident::ACCIDENT_INFRASTRUCTURE_ROAD,
@@ -122,23 +129,26 @@ class IncidentType extends AbstractType
             )
             ->add('accidentOpponent', ChoiceType::class,
                 [
-                'choices' => [
-                    '' => null,
-                    'Fußgänger' => Incident::ACCIDENT_OPPONENT_PEDESTRIAN,
-                    'Fahrradfahrer' => Incident::ACCIDENT_OPPONENT_CYCLIST,
-                    'Motorradfahrer' => Incident::ACCIDENT_OPPONENT_MOTORCYCLE,
-                    'Personenkraftwagen' => Incident::ACCIDENT_OPPONENT_CAR,
-                    'Lastkraftwagen' => Incident::ACCIDENT_OPPONENT_TRUCK,
-                    'Traktor oder landwirtschaftliches Fahrzeug' => Incident::ACCIDENT_OPPONENT_TRACTOR,
-                    'Eisenbahn' => Incident::ACCIDENT_OPPONENT_TRAIN,
-                    'Straßenbahn' => Incident::ACCIDENT_OPPONENT_TRAM,
-                    'Tier, Haustier' => Incident::ACCIDENT_OPPONENT_ANIMAL,
-                    'keiner / Alleinunfall' => Incident::ACCIDENT_OPPONENT_NONE,
-                    'unbekannt' => Incident::ACCIDENT_OPPONENT_UNKNOWN,
+                    'required' => false,
+                    'choices' => [
+                        '' => null,
+                        'Fußgänger' => Incident::ACCIDENT_OPPONENT_PEDESTRIAN,
+                        'Fahrradfahrer' => Incident::ACCIDENT_OPPONENT_CYCLIST,
+                        'Motorradfahrer' => Incident::ACCIDENT_OPPONENT_MOTORCYCLE,
+                        'Personenkraftwagen' => Incident::ACCIDENT_OPPONENT_CAR,
+                        'Lastkraftwagen' => Incident::ACCIDENT_OPPONENT_TRUCK,
+                        'Traktor oder landwirtschaftliches Fahrzeug' => Incident::ACCIDENT_OPPONENT_TRACTOR,
+                        'Eisenbahn' => Incident::ACCIDENT_OPPONENT_TRAIN,
+                        'Straßenbahn' => Incident::ACCIDENT_OPPONENT_TRAM,
+                        'Tier, Haustier' => Incident::ACCIDENT_OPPONENT_ANIMAL,
+                        'keiner / Alleinunfall' => Incident::ACCIDENT_OPPONENT_NONE,
+                        'unbekannt' => Incident::ACCIDENT_OPPONENT_UNKNOWN,
+                    ]
                 ]
-            ])
+            )
             ->add('accidentSex', ChoiceType::class,
                 [
+                    'required' => false,
                     'choices' => [
                         '' => null,
                         'männlich' => Incident::ACCIDENT_SEX_MALE,
@@ -146,9 +156,10 @@ class IncidentType extends AbstractType
                     ]
                 ]
             )
-            ->add('accidentAge', NumberType::class)
+            ->add('accidentAge', NumberType::class, ['required' => false])
             ->add('accidentPedelec', ChoiceType::class,
                 [
+                    'required' => false,
                     'choices' => [
                         'unbekannt' => null,
                         'ja' => true,
@@ -158,6 +169,7 @@ class IncidentType extends AbstractType
             )
             ->add('accidentHelmet', ChoiceType::class,
                 [
+                    'required' => false,
                     'choices' => [
                         'unbekannt' => null,
                         'ja' => true,
@@ -167,6 +179,7 @@ class IncidentType extends AbstractType
             )
             ->add('accidentCyclistCaused', ChoiceType::class,
                 [
+                    'required' => false,
                     'choices' => [
                         'unbekannt' => null,
                         'ja' => true,
